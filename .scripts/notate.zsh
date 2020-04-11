1607ba997ef742a78a03523831c6a0c8149c0ec4
Gregor <crap@wbrck.de>
2020-04-11 21:44:41 +0200

    notate bugs fix

--- .scripts/notate.zsh
+++ .scripts/notate.zsh
@@ -11,8 +11,8 @@ get_bib() {
 }
 
 get_old_info() {
-    infos=$(pcregrep -M -v "^~.*|^@.*|(^\t\w+\s=\s.*)|^\s\s\s\s\w+\s=\s.*|^}" "$old_info_md"".md")
-    tags=$(pcregrep -M "(?<=\stitle = {).*?(?=(})" "$old_info_md"".md")
+    infos=$(pcregrep -M -v "^~.*|^@.*|(^\t\w+\s=\s.*)|^\s\s\s\s\w+\s=\s.*|^}" "$old_info_md.md")
+    tags=$(pcregrep -M "(?<=\stitle = {).*?(?=(}))" "$old_info_md.md")
 }
 
 make_file() {
@@ -31,6 +31,7 @@ make_file() {
 if [ -f "$1" ]; then
         doi=$(pdfinfo "$1" | grep -io "doi:.*") ||
         doi=$(pdftotext "$1" - | pcregrep -M -o "\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![\"&\'<>])\S)+)\b" | head -n 1) ||
+        exit 1
         pre_file_name=$(echo $1)
         old_info_md=$(echo $pre_file_name | pcregrep -o "^.*(?=\.)")
         get_bib
@@ -38,7 +39,6 @@ if [ -f "$1" ]; then
         make_file
         mv "$pre_file_name" "$key"".pdf"
         rm "$old_info_md"".md"
-        exit 1
 else
         doi="$1"
         get_bib
