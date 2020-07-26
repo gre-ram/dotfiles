70a22026a5b65f5e28b55ad39326dfbf7f7f2f9c
Gregor <crap@wbrck.de>
2020-07-26 17:11:53 +0200

    emacs tweeks

--- Emacs/.doom.d/init.el
+++ Emacs/.doom.d/init.el
@@ -42,7 +42,6 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold
-       format-all
        (format +onsave)  ; automated prettiness
        multiple-cursors  ; editing in many places at once
        snippets          ; my elves. They type so I don't have to
