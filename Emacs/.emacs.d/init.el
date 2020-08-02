ff7512cffbe30fcd0e882582b278573830342c23
Gregor <crap@wbrck.de>
2020-08-02 18:39:04 +0200

    added ivy instead of helm

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -15,17 +15,4 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/dotfiles/Emacs/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (doom-modeline nord-theme yaml-mode use-package rainbow-delimiters racket-mode python-mode projectile paredit org-plus-contrib org-bullets multi-term htmlize helpful graphviz-dot-mode gnuplot git-timemachine forge flycheck-package evil-surround evil-org evil-magit evil-collection elpy eglot dumb-jump diff-hl company-restclient company-coq avy auto-compile ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
