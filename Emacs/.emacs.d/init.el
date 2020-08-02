c1712d9b19c760d8678d65e6bd9fb3dfb4571cb3
Gregor <crap@wbrck.de>
2020-08-02 19:23:56 +0200

    remove orf-ref becuase its already installed

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -16,3 +16,17 @@
 
 (org-babel-load-file "~/dotfiles/Emacs/configuration.org")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (citeproc-org magit diff-hl smex flx counsel which-key doom-modeline nord-theme evil-org evil-surround evil-collection evil auto-compile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
