44182c9a0268b3f8466c1b7654eb40f22b19b637
Gregor <crap@wbrck.de>
2020-10-25 19:44:19 +0100

    update

--- Doom/.doom.d/config.el
+++ Doom/.doom.d/config.el
@@ -3,16 +3,64 @@
 (setq doom-theme 'doom-one)
 (setq display-line-numbers-type 'relative)
 
-
+(setq org-directory "/Users/gregor/Documents/ORG/")
 (setq! +biblio-pdf-library-dir "/Users/gregor/Documents/ORG/pdfs/"
        +biblio-default-bibliography-files '("/Users/gregor/Documents/ORG/main.bib")
-       +biblio-notes-path "/Users/gregor/Documents/ORG/notes/")
+       +biblio-notes-path "/Users/gregor/Documents/ORG/roam/")
 
 
-(setq   bibtex-autokey-year-length 4
-	bibtex-autokey-name-year-separator "-"
-	bibtex-autokey-year-title-separator "-"
-	bibtex-autokey-titleword-separator "-"
-	bibtex-autokey-titlewords 1
-	bibtex-autokey-titlewords-stretch 1
-	bibtex-autokey-titleword-length 20)
+(setq! orb-preformat-keywords
+        '("=key=" "title" "url" "file" "author-or-editor" "keywords"))
+  (setq! orb-templates
+        '(("r" "ref" plain (function org-roam-capture--get-point)
+           ""
+           :file-name "literature/%<%Y-%m-%d-%H-%M-%S>-${slug}"
+           :head "#+TITLE: ${=key=}: ${title}
+#+ROAM_KEY: ${ref}
+#+ROAM_TAGS:
+Time-stamp: <>
+- tags :: ${keywords}
+
+* ${title}
+  :PROPERTIES:
+  :Custom_ID: ${=key=}
+  :URL: ${url}
+  :AUTHOR: ${author-or-editor}
+  :NOTER_DOCUMENT: %(orb-process-file-field \"${=key=}\")
+  :NOTER_PAGE:
+  :END:
+
+"
+
+           :unnarrowed t)))
+
+
+(setq!
+   bibtex-completion-pdf-field "file"
+   bibtex-completion-notes-template-multiple-files
+   (concat
+    "#+TITLE: ${title}\n"
+    "#+ROAM_KEY: cite:${=key=}"
+    "#+ROAM_TAGS: ${keywords}"
+    "#+CREATED:%<%Y-%m-%d-%H-%M-%S>"
+    "Time-stamp: <>\n"
+    "- tags :: \n"
+    "* NOTES \n"
+    ":PROPERTIES:\n"
+    ":Custom_ID: ${=key=}\n"
+    ":NOTER_DOCUMENT: %(orb-process-file-field \"${=key=}\")\n"
+    ":AUTHOR: ${author-abbrev}\n"
+    ":JOURNAL: ${journaltitle}\n"
+    ":DATE: ${date}\n"
+    ":YEAR: ${year}\n"
+    ":DOI: ${doi}\n"
+    ":URL: ${url}\n"
+    ":END:\n\n"
+    )
+)
+
+(setq!
+         org-ref-completion-library 'org-ref-ivy-cite
+         org-ref-note-title-format "* NOTES %y - %t\n :PROPERTIES:\n  :Custom_ID: %k\n  :NOTER_DOCUMENT: %F\n :ROAM_KEY: cite:%k\n  :AUTHOR: %9a\n  :JOURNAL: %j\n  :YEAR: %y\n  :VOLUME: %v\n  :PAGES: %p\n  :DOI: %D\n  :URL: %U\n :END:\n\n"
+         org-ref-notes-function 'orb-edit-notes
+    )
