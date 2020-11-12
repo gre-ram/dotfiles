53b216db8d68a21802f38fb32009dc50d238a07c
Gregor <crap@wbrck.de>
2020-11-12 18:35:56 +0100

    minimized

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -1,6 +1,6 @@
 (setq custom-file "~/.emacs-custom")
 (if (file-exists-p custom-file)
-    (load custom-file))
+  (load custom-file))
 
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
@@ -11,5 +11,11 @@
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package auto-compile
+             :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
 
 (org-babel-load-file "~/dotfiles/Emacs/configuration.org")
