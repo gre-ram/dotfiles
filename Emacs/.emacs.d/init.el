bcda7a658798e1f5c2d3a846d29b626f2710e4bf
Gregor <crap@wbrck.de>
2021-05-23 15:04:15 +0200

    using straight

--- Emacs/.emacs.d/init.el
+++ Emacs/.emacs.d/init.el
@@ -2,20 +2,27 @@
 (if (file-exists-p custom-file)
   (load custom-file))
 
-(require 'package)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
+(setq package-enable-at-startup nil)
+(setq straight-use-package-by-default t)
 
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-(use-package auto-compile
-             :config (auto-compile-on-load-mode))
-(setq load-prefer-newer t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+
+(use-package org-mode
+  :hook (org-mode . efs/org-mode-setup))
 
 (org-babel-load-file "~/dotfiles/Emacs/configuration.org")
